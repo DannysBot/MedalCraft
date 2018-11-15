75989b3fb5abe03056670ce7495a177568a40e32
DannysBot <45067457+DannysBot@users.noreply.github.com>
2018-11-15 14:15:55 +0000

    Update MedalCraft.js

--- MedalCraft.js
+++ MedalCraft.js
@@ -1,14 +1,19 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('ready', () => {
-   console.log('sunt gata!');
+const TOKEN = "NTEyNjA0MzM1ODI2MjcyMjY1.Ds8RBw.iAHSFw8GEnEVcYwKjBCkD8H4gb0";
+
+var bot = new Discord.Client();
+
+bot.on('ready', function() => {
+  console.log("Ready");
 });
 
-client.on('message', message => {
-   if (message.content === 'ping') {
-     message.reply('pong');
-   }
+bot.on('message', function(message) => {
+  if (message.author.equals(bot.user)) return;
+  
+  if (message.content == "hello") {
+      message.channel.sendMessage("Hi, there")
+  }
 });
 
-client.login(process.env.BOT_TOKEN);
+bot.login(TOKEN);
